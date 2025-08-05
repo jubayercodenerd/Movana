e507c9ab3482c0336f6cdaf0d8630ca0dfdcbda5
Jubayer <amd.jubayer.cse@gmail.com>
2025-08-06 03:12:29 +0600

    centered the grid

--- src/HomePage/CardGrid.jsx
+++ src/HomePage/CardGrid.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CardGrid = ({children}) => {
     return (
-        <div className={"z-50 my-8 w-[95%] md:w-[90%] lg:w-[90%] xl:w-[80%] gap-3 md:gap-5 xl:gap-8 grid " +
+        <div className={"z-50 my-8 w-[95%] justify-center md:w-[90%] lg:w-[90%] xl:w-[80%] gap-3 md:gap-5 xl:gap-8 grid " +
             "grid-cols-[repeat(auto-fit,minmax(140px,1fr))] " +
             "sm:grid-cols-[repeat(auto-fit,minmax(160px,.8fr))] " +
             "md:grid-cols-[repeat(auto-fit,minmax(180px,.8fr))] " +
