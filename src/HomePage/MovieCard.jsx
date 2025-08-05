30bcf9227164c35038aa7c0f467009b32cd570cb
Jubayer <amd.jubayer.cse@gmail.com>
2025-08-06 03:14:18 +0600

    centered the grid

--- src/HomePage/MovieCard.jsx
+++ src/HomePage/MovieCard.jsx
@@ -9,7 +9,7 @@ const MovieCard = ({movieId, setCurrentMovie, poster, rating, title, date, lang,
     return (
         <Link to={"/player"}>
             <div onClick={() => {setCurrentMovie(movieId);}} className={"z-0 w-full p-[8px] rounded-lg bg-gradient-to-br from-purple-950 to-[rgba(0,0,255,.13)] max-md:h-[290px] max-md:w-[150px] max-md:min-w-[140px] flex flex-col items-start justify-between max-md:p-[4px]"}>
-                <img className={"max-h-full"} src={poster?`https://image.tmdb.org/t/p/w500/${poster}`:'/project-images/No-Poster.png' } alt="poster"/>
+                <img className={"max-h-full rounded-md"} src={poster?`https://image.tmdb.org/t/p/w500/${poster}`:'/project-images/No-Poster.png' } alt="poster"/>
                 <div className={"flex flex-col items-start justify-between"}>
                     <div className={"flex flex-wrap justify-start items-center my-[3px] gap-[2px]"}>
                         <p className={"text-[13px] max-md:text-[11px] font-semibold text-gray-300"}>{title}</p>
